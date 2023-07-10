110fd9479c76592e7eb261c302f939fdb0d7778c
root <root@HOME-PC.localdomain>
2023-07-10 15:00:30 +0400

    5 Шаг

---
+++ src/even.js
@@ -0,0 +1,18 @@
+import runGame from '../index.js';
+import {
+	  getRandomNumber,
+	  isEvenNumber,
+} from '../utils.js';
+
+const description = 'Answer "yes" if the number is even, otherwise answer "no".';
+
+const getQuestionAndAnswer = () => {
+	  const question = getRandomNumber(0, 100);
+	  const correctAnswer = isEvenNumber(question) ? 'yes' : 'no';
+
+	  return [question, correctAnswer];
+};
+
+export default () => {
+	  runGame(description, getQuestionAndAnswer);
+};
