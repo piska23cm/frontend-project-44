f9ee6320810ad015c53c91a9eae50923caef54a9
root <root@HOME-PC.localdomain>
2023-08-06 18:39:23 +0400

    fix code 15

--- src/cli.js
+++ src/cli.js
@@ -1,9 +1,7 @@
- /*global console*/
-
 import readlineSync from 'readline-sync';
 
 export default () => {
- console.log('Welcome to the Brain Games!'); // eslint-disable-line no-console
- const name = readlineSync.question('May I have your name? ');
- console.log(`Hello, ${name}!`);
+  console.log('Welcome to the Brain Games!'); // eslint-disable-line no-console
+  const name = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${name}!`);
 };
