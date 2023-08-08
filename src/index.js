37689ecfc824c015fc8b4c26f109aa54cc8243d9
root <root@HOME-PC.localdomain>
2023-08-08 15:13:16 +0400

    fix code20

--- src/index.js
+++ src/index.js
@@ -7,15 +7,15 @@ export default (description, generateRound) => {
   const playerName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${playerName}!`);
   console.log(description);
-   for (let i = 1; i <= attemptsCount; i += 1) {
-     const [question, correctAnswer] = generateRound();
-     console.log(`Question: ${question}`);
-     const playerAnswer = readlineSync.question('Your answer: ');
-       if (playerAnswer !== correctAnswer) {
-         console.log(`"${playerAnswer}" is wrong answer ;(. Correct answer was "${correctAnswer}".`);
-         console.log(`Let's try again, ${playerName}!`);
-       return;
-       }
+  for (let i = 1; i <= attemptsCount; i += 1) {
+    const [question, correctAnswer] = generateRound();
+    console.log(`Question: ${question}`);
+    const playerAnswer = readlineSync.question('Your answer: ');
+    if (playerAnswer !== correctAnswer) {
+      console.log(`"${playerAnswer}" is wrong answer ;(. Correct answer was "${correctAnswer}".`);
+      console.log(`Let's try again, ${playerName}!`);
+      return;
+         }
   console.log('Correct!');
   }
   console.log(`Congratulations, ${playerName}!`);
