41926b5016e0d840eaebe10650a34206f0216e05
root <root@HOME-PC.localdomain>
2023-08-10 20:08:02 +0400

    Проверка 7

--- src/games/calc.js
+++ src/games/calc.js
@@ -8,27 +8,27 @@ const minRange = 0;
 const maxRange = 50;
 
 const calculate = (x, y, operator) => {
-	  switch (operator) {
-		      case '+':
-			        return x + y;
-			      case '-':
-			        return x - y;
-			      case '*':
-			        return x * y;
-			      default:
-			        throw new Error(`There is no such operator like '${operator}'!`);
-			    }
+  switch (operator) {
+  case '+':
+  return x + y;
+  case '-':
+  return x - y;
+  case '*':
+  return x * y;
+  default:
+  throw new Error(`There is no such operator like '${operator}'!`);
+  }
 };
 
 const generateRound = () => {
-	  const number1 = getRandomNumber(minRange, maxRange);
-	  const number2 = getRandomNumber(minRange, maxRange);
-	  const operator = operators[getRandomIndex(operators)];
-	  const question = `${number1} ${operator} ${number2}`;
-	  const correctAnswer = calculate(number1, number2, operator).toString();
-	  return [question, correctAnswer];
+  const number1 = getRandomNumber(minRange, maxRange);
+  const number2 = getRandomNumber(minRange, maxRange);
+  const operator = operators[getRandomIndex(operators)];
+  const question = `${number1} ${operator} ${number2}`;
+  const correctAnswer = calculate(number1, number2, operator).toString();
+  return [question, correctAnswer];
 };
 
 export default () => {
-	  run(description, generateRound);
+  run(description, generateRound);
 };
